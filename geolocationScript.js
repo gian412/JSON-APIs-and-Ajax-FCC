09ed21d8459f68a89443dfac5419e6b81524bfd5
gian412 <gian.regis@gmail.com>
2020-10-13 17:11:42 +0200

    Geolocation

---
+++ geolocationScript.js
@@ -0,0 +1,9 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    document.getElementById("data").innerHTML =
+      "latitude: " +
+      position.coords.latitude +
+      "<br>longitude: " +
+      position.coords.longitude;
+  });
+}
